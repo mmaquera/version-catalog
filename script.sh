9dd721810bc874b516fb41bddd7ea6b7b0ae4f33
Marco <marco@Marcos-MacBook-Pro.local>
2022-11-28 20:05:54 -0500

    feat: add pluging and firt version

---
+++ script.sh
@@ -0,0 +1,7 @@
+echo "##################### Script ###################"
+echo " run script in version-catalog ..."
+
+./gradlew generateCatalogAsToml
+
+echo "completed script"
+echo "##################### Script ###################"
